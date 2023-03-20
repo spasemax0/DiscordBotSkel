d4f87843d5b3cddad7ec6eac28453f4869679068
spasemax0 <123032891+spasemax0@users.noreply.github.com>
2023-03-19 19:02:53 -0600

    Update responses.py

--- responses.py
+++ responses.py
@@ -1,4 +1,5 @@
 #Author: spasemax0
+#this is a responses page used to call the bot and its events
 import random
 import randfacts
 import googletrans
@@ -15,10 +16,8 @@ def get_response(message: str) -> str:
     if message == '$roll':
         return str(random.randint(1, 6))
 
-    if message == '$translate':
-        return str(result = translator.translate(input))
-
     if p_message == '!buy puffer fish':
         return '`no you have no money`'
+    
     if p_message == '!8ball':
         return 'magic_eightball'
